7ee712bc8de61608c0e7af5768b523fdcf609f30
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-09-04 19:09:12 +1000

    Fix Comments Endpoint

--- src/server.js
+++ src/server.js
@@ -19,22 +19,31 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get ('/api/articles/:name', async (req, res) => {
+const withDB = async (operations, res) => {
     try {
-        const articleName = req.params.name;
         const client = await MongoClient.connect('mongodb://localhost:27017', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         const db = client.db("e-zine_db");
-        const articleInfo = await db
-            .collection('articles')
-            .findOne({name: articleName})
-        res.status(200).json(articleInfo);
+        await operations(db);
         client.close();
-    } catch(error) {
+    } catch (error) {
         res.status(500).json({message: 'Error connecting to db', error });
     }
+};
+
+app.get ('/api/articles/:name', async (req, res) => {
+        withDB(async (db) => {
+            const articleName = req.params.name;
+
+            const articleInfo = await db
+                .collection('articles')
+                .findOne({name: articleName})
+            res.status(200).json(articleInfo);
+    }, res);
+
+
 });
 
 //Testing:
@@ -46,8 +55,21 @@ app.post('/api/articles/:name/add-comments', (req, res) => {
     const {username, text} = req.body
     const articleName = req.params.name
 
-    articlesInfo[articleName].comments.push({ username, text });
-    res.status(200).send(articlesInfo[articleName]);
+    withDB(async (db) => {
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        await db.collection('articles').updateOne(
+            {name : articleName},
+            {
+                $set: {
+                    comments: articleInfo.comments.concat({username, text}),
+                },
+            }
+        );
+        const updatedArticleInfo = await db
+            .collection("articles")
+            .findOne({name: articleName });
+            res.status(200).json(updatedArticleInfo);
+    }, res);
 });
 
 app.listen (8000, () => console.log("listening on port 8000"));
